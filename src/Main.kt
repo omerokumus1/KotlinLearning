acb1f67702e9aa4cf55ed5b0209d1c15b2bf398a
Ömer Okumuş <omerokumus3@gmail.com>
2024-10-12 09:59:43 +0300

    selaed classes

--- src/Main.kt
+++ src/Main.kt
@@ -27,12 +27,46 @@ class AnotherError : DBError() {
     }
 }
 
-class OtherError: DBError() {
+class OtherError : DBError() {
     fun printError() {
         println("Other Error")
     }
 }
 
-class Cls: Err(2) {
+class Cls : Err(2) {
 
+}
+
+sealed class UserEvent {
+    data object OnClick : UserEvent() {
+        val x = 0
+        val y = 0
+    }
+
+    data object OnLongClick : UserEvent() {
+        val duration = 1000
+    }
+
+    data object OnSwipe : UserEvent() {
+        val direction = "left"
+    }
+}
+
+sealed class AddVehicleFormRow(val submitKey: String, val type: RowType) {
+    data class VehicleName(val queryMapKey: String) : AddVehicleFormRow("VehicleName", RowType.Input)
+    data class VehiclePlate(val queryMapCityKey: String, val queryMapPlateKey: String, val queryMapNumberKey: String) :
+        AddVehicleFormRow("VehiclePlate", RowType.PlateInput)
+
+    data class VehicleType(val queryMapKey: String) : AddVehicleFormRow("VehicleType", RowType.Picker)
+    data class VehicleBrand(val queryMapKey: String) : AddVehicleFormRow("VehicleBrand", RowType.Picker)
+    data class VehicleModel(val queryMapKey: String) : AddVehicleFormRow("VehicleModel", RowType.Picker)
+    data class VehicleYear(val queryMapKey: String) : AddVehicleFormRow("VehicleYear", RowType.Picker)
+    data class AdditionalInfoSwitch(val queryMapKey: String) : AddVehicleFormRow("AdditionalInfoSwitch", RowType.Switch)
+    data class KaskoValidityDate(val queryMapKey: String) : AddVehicleFormRow("KaskoValidityDate", RowType.DatePicker)
+    data object ConfirmButton : AddVehicleFormRow("ConfirmButton", RowType.Button)
+    data object DeleteButton : AddVehicleFormRow("DeleteButton", RowType.Button)
+}
+
+enum class RowType {
+    Input, PlateInput, Picker, Switch, DatePicker, Button
 }
