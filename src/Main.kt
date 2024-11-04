983191c1cae5821f0d821f6ee36037e00c6bc318
Ömer Okumuş <omerokumus3@gmail.com>
2024-11-04 08:46:53 +0300

    overloading Unary operators

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,47 @@
 fun main() {
-    println("Hello World!")
+    val p1 = Point(0, 1)
+    val p2 = Point(1, 2)
+
+    // ? Add two Point objects
+    println(p1 + p2)
+
+    // ? Unary plus operator
+    val s = Shape().apply {
+        +Point(0, 0)
+        +Point(1, 1)
+        +Point(2, 2)
+        +Point(3, 4)
+    }
+    println(s)
+
+    val s1 = shape {
+        +Point(0, 0)
+        +Point(1, 1)
+        +Point(2, 2)
+        +Point(3, 4)
+    }
+
+}
+
+data class Point(val x: Int, val y: Int)
+
+// ? Overload the + operator
+operator fun Point.plus(other: Point) = Point(x + other.x, y + other.y)
+
+
+// ? Overload unary plus operator (The same applies to unary minus, increment, decrement, and not operators)
+class Shape {
+    private val points = mutableListOf<Point>()
+
+    operator fun Point.unaryPlus() {
+        points.add(this)
+    }
+}
+
+// * Shape DSL
+fun shape(init: Shape.() -> Unit): Shape {
+    val shape = Shape()
+    shape.init()
+
+    return shape
 }
