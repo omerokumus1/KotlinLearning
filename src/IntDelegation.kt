be5cf3ed835684762901eef45fdb110d8b257a17
Ömer Okumuş <omerokumus3@gmail.com>
2024-11-08 08:49:30 +0300

    using public functions & properties

--- src/IntDelegation.kt
+++ src/IntDelegation.kt
@@ -3,13 +3,20 @@ import kotlin.reflect.KProperty
 
 class IntDelegation(private val defValue: Int? = null) :
     ReadOnlyProperty<MyClass, Int> {
-    private var readCount = 0
+
+    private var _readCount = 0
+    val readCount: Int get() = _readCount
+
     override fun getValue(
         thisRef: MyClass,
         property: KProperty<*>
     ): Int {
-        readCount++
+        _readCount++
         return defValue ?: 42
+    }
+
+    fun resetReadCount() {
+        _readCount = 0
     }
 }
 
--- src/MyClass.kt
+++ src/MyClass.kt
@@ -1,8 +1,13 @@
 class MyClass {
-    val myNumber: Int by IntDelegation(23)
+    val intDelegation = IntDelegation(23)
+    val myNumber: Int by intDelegation
+
+//    val myNumber: Int by IntDelegation(23)
 
     fun test() {
         println(myNumber)
+        println(intDelegation.readCount)
+        intDelegation.resetReadCount()
     }
 
 }
